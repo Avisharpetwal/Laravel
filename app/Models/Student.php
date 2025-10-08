5366a4b14364b4b6cdd8f9f7cf8095af28f21105
Avishar37 <avisharpetwal37@gmail.com>
2025-10-08 18:18:49 +0530

    Model for Student table

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
+
 {
-    //
+    //For if Models not match for database model name 
+    // protected $table="collge_students";
 }
