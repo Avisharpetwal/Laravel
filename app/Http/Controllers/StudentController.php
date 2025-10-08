01d6386b4a566c6dfbe6d855fed7b3d673c6275a
Avishar37 <avisharpetwal37@gmail.com>
2025-10-08 18:02:59 +0530

    Create Controller For Student Table In database

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    //
+    function getStudents(){
+
+        $students=\App\Models\Student::all();
+        // return   $students;
+        return view('students',['data'=>$students]);
+    }
+}
