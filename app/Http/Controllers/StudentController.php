905dad074397b70031c5dd409be3f8e5d8ba212c
Avishar37 <avisharpetwal37@gmail.com>
2025-10-09 18:22:43 +0530

    Insert Data In MySQL With HTML Form

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Student;
 
 class StudentController extends Controller
 {
@@ -12,5 +13,24 @@ class StudentController extends Controller
         $students=\App\Models\Student::all();
         // return   $students;
         return view('students',['data'=>$students]);
+    }
+
+
+    function add(Request $request){
+
+        $students=new Student();
+        $students->name=$request->name;
+        $students->email=$request->email;
+        $students->phone=$request->phone;
+        $students->save();
+
+        if($students){
+            echo"Added SuccessFully";
+        }
+        else{
+            echo"Not Added yet";
+        }
+        // return $request;
+        // return" Student Added SuccessFully";
     }
 }
