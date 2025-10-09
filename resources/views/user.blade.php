92dfc4f9060e98434523d500e393d1dbaa6e3d7b
Avishar37 <avisharpetwal37@gmail.com>
2025-10-09 11:27:24 +0530

    Query Builder

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,1 +1,49 @@
+{{--//For Http client
 <h1>View Page </h1>
+
+<ul>
+    <li>
+        <span>Id:</span><span><b>{{$data->id}}</b></span>
+    </li> 
+    <li>   
+        <span>Name:</span><span>{{$data->username}}</span>
+    </li>
+    <li>
+        <span>Email:</span><span>{{$data->email}}</span>
+    </li>
+    <li>
+        <span>Phone:</span><span>{{$data->phone}}</span>
+    </li>
+</ul>
+     
+</div>
+--}}
+
+<div>
+    <h1>Users Details</h1>
+    <table border="10">
+        <tr>
+            <td>Id</td>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Phone</td>
+        </tr>
+        <tr>
+            @foreach($xyz as $a)
+            <tr>
+             <td>{{$a->id}}</td>
+             <td>{{$a->name}}</td>
+             <td>{{$a->email}}</td>
+             <td>{{$a->phone}}</td>
+            </tr>
+        </tr>
+        @endforeach
+
+
+        </table>
+     
+</div>
+
+
+
+
