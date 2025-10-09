4265645a526e17a50132aa9c30fcf925e57d0344
Avishar37 <avisharpetwal37@gmail.com>
2025-10-09 13:04:06 +0530

    Show  Data of ELoquent Model:Query Builder

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -1,4 +1,5 @@
-<div>
+{{--
+    <div>
     <h1>Users Details</h1>
     <table border="10">
         <tr>
@@ -22,3 +23,29 @@
         </table>
      
 </div>
+--}}
+<h1>ELoquent Model:Query Builder</h1>
+<!-- {{print_r($users)}} -->
+
+<div>
+    <h1>Users Details</h1>
+    <table border="10">
+        <tr>
+            <td>Id</td>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Phone</td>
+        </tr>
+        <tr>
+            @foreach($users as $a)
+            <tr>
+             <td>{{$a->id}}</td>
+             <td>{{$a->name}}</td>
+             <td>{{$a->email}}</td>
+             <td>{{$a->phone}}</td>
+            </tr>
+        </tr>
+        @endforeach
+
+
+        </table>
