95d77b3c916cd45947cffe92d5a224860a014f83
Avishar37 <avisharpetwal37@gmail.com>
2025-10-10 18:20:47 +0530

    Add Pagination

--- resources/views/list-student.blade.php
+++ resources/views/list-student.blade.php
@@ -3,7 +3,7 @@
 <h1>Student Details </h1>
      <table border="10">
         <tr>
-            <td>Id</td>
+            <!-- <td>Id</td> -->
             <td>Name</td>
             <td>Email</td>
             <td>Phone</td>
@@ -14,7 +14,7 @@
         <tr>
             @foreach($studentd as $d)
             <tr>
-             <td>{{$d->id}}</td>
+             <!-- <td>{{$d->id}}</td> -->
              <td>{{$d->name}}</td>
              <td>{{$d->email}}</td>
              <td>{{$d->phone}}</td>
@@ -29,4 +29,15 @@
         @endforeach
  
 
-        </table></div>
+        </table>
+        {{$studentd->links()}}
+    
+    </div>
+
+    <style>
+
+        .w-5.h-5{
+            width: 20px;
+        }
+    </style>
+
